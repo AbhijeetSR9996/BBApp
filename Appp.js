b8b4105adb03a9aa2c4e386a43e000ba44942256
Abhijeet Singh Rathour <47415323+AbhijeetSR9996@users.noreply.github.com>
2022-09-16 19:16:44 +0530

    Update Appp.js

--- Appp.js
+++ Appp.js
@@ -4,17 +4,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Screen1 from './screens/Screen1';
 import Screen2 from './screens/Screen2';
 import Screen3 from './screens/Screen3';
+import ScreenSplash from './screens/ScreenSplash';
 
 const Stack = createNativeStackNavigator();
 
 export default function Appp() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Screen2"
+            <Stack.Navigator initialRouteName="ScreenSplash"
                 screenOptions={{ headerShown: false }}>
                 <Stack.Screen name='Screen1' component={Screen1} />
                 <Stack.Screen name='Screen2' component={Screen2} />
                 <Stack.Screen name='Screen3' component={Screen3} />
+                <Stack.Screen name='ScreenSplash' component={ScreenSplash} />
             </Stack.Navigator>
         </NavigationContainer>
     );
